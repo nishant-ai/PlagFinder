8a1cf715126bf0b10d28b1f16add311a8f4ea3b1
Nishant Sharma <nishant-ai@outlook.com>
2022-01-16 14:08:53 +0530

    nltk download

--- core/helpers.py
+++ core/helpers.py
@@ -68,7 +68,10 @@ import pandas as pd
 import numpy as np
 import unicodedata
 import nltk
-nltk.download()
+
+nltk.download('punkt')
+nltk.download('wordnet')
+
 
 stopwords = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
 
@@ -109,7 +112,6 @@ def frequencyMatch(df_scraped , df_test):
     merged.frequency_match = np.where(merged.frequency_match < 0, 0, merged.frequency_match)
     merged.frequency_match = np.where(merged.frequency_match > 100, 100, merged.frequency_match)
     merged = merged.drop(merged[merged.frequency_test == 0].index)
-#     print(merged)
     return merged['frequency_match'].mean()
 
 
